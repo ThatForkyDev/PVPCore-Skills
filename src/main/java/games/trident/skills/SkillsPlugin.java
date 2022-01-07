9a445b2a77f9ed491d4779d6045ef72b0e2e7fd3
ForkyDev <declaredmc@gmail.com>
2022-01-07 05:09:30 +0000

    Config design.

---
+++ src/main/java/games/trident/skills/SkillsPlugin.java
@@ -0,0 +1,10 @@
+package games.trident.skills;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkillsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
