86987d232b875ac4f62d70354ce5b691dd8190e9
ForkyDev <declaredmc@gmail.com>
2022-01-07 11:46:33 +0000

    dev

--- src/main/java/games/trident/skills/listeners/FishingListeners.java
+++ src/main/java/games/trident/skills/listeners/FishingListeners.java
@@ -1,10 +1,15 @@
 package games.trident.skills.listeners;
 
 import games.trident.skills.SkillsPlugin;
+import net.minecraft.server.v1_8_R3.EntityFishingHook;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
+import org.bukkit.entity.FishHook;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+
+import java.lang.reflect.Field;
 
 public class FishingListeners implements Listener {
     private SkillsPlugin plugin;
@@ -17,8 +22,33 @@ public class FishingListeners implements Listener {
 
     @EventHandler
     public void onFish(PlayerFishEvent event) {
-        event.getPlayer().sendMessage("Bite Chance: " + event.getHook().getBiteChance());
-
-        event.getHook().setBiteChance(0.9);
+        setBiteTime(event.getHook(), 4);
     }
+
+    private void setBiteTime(FishHook hook, int time) {
+        net.minecraft.server.v1_8_R3.EntityFishingHook hookCopy = (EntityFishingHook) ((CraftEntity) hook).getHandle();
+
+        Field fishCatchTime = null;
+
+        try {
+            fishCatchTime = net.minecraft.server.v1_8_R3.EntityFishingHook.class.getDeclaredField("aw");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        fishCatchTime.setAccessible(true);
+
+        try {
+            fishCatchTime.setInt(hookCopy, time);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        fishCatchTime.setAccessible(false);
+    }
+
 }
