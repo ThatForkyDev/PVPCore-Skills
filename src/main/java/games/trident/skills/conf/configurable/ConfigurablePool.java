0e3184a8f1c817472b08a41e312298884cde111c
ForkyDev <declaredmc@gmail.com>
2022-01-07 06:22:43 +0000

    Conf pool

--- src/main/java/games/trident/skills/conf/configurable/ConfigurablePool.java
+++ src/main/java/games/trident/skills/conf/configurable/ConfigurablePool.java
@@ -1,16 +1,20 @@
 package games.trident.skills.conf.configurable;
 
 import com.google.common.collect.Lists;
+import lombok.Getter;
 import org.bukkit.configuration.ConfigurationSection;
 
 import java.util.List;
 
+@Getter
 public class ConfigurablePool {
     private int level;
     private int interval;
-    private List<String> possibleRewards = Lists.newArrayList();
+    private List<String> possibleRewards;
 
     public ConfigurablePool(ConfigurationSection section) {
-
+        this.level = section.getInt("level");
+        this.interval = section.getInt("interval");
+        this.possibleRewards = section.getStringList("rewards");
     }
 }
